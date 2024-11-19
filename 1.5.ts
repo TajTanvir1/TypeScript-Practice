4aa84ffef91013247e12cb6a6cc7fb5ac061a352
Tanvir Ahamed <155292317+TajTanvir1@users.noreply.github.com>
2024-11-19 16:42:16 +0600

    Object types

---
+++ 1.5.ts
@@ -0,0 +1,22 @@
+// Object
+
+{
+    //
+
+    const user :{
+        company: 'City Trading'; //literal type, which mean have to write same words. when we need fixed value
+        readonly firstName: string;
+        midName?: string; //optional type
+        lastName: string;
+    } = {
+        company: 'City Trading',
+        firstName: 'Tanvir',
+        midName : 'Ahamed',
+        lastName : 'Jubayer'
+    }
+
+    // user.firstName= 'Something'; //Readonly files can't be changeable
+
+
+    //
+}
