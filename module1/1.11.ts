0507cccdd538ee2bdf2e00575b110ef4726de7ef
tajtanvvir1 <tanvirah1223@gmail.com>
2024-11-21 16:20:18 +0600

    Ternary and Nullish

---
+++ module1/1.11.ts
@@ -0,0 +1,52 @@
+{
+// Ternary Operator || Optional chaining || nullish coalescing
+
+const age: number = 18;
+
+if(age >= 18){
+    // console.log("Adult");
+} else{
+    console.log("Not Adult");
+}
+
+const isAdult = age>= 18? "adult" : "not adult";
+// console.log(isAdult);
+
+// Nullish coalescing operator -- ??
+// decision make for -> null / undefined
+
+const isAuthenticated = null;
+const isAuthenticated2 = undefined;
+
+const result1 = isAuthenticated ?? "Guest";
+const result2 = isAuthenticated2 ?? "Guest";
+// console.log({result1}, {result2});
+
+
+type User = {
+    name: string;
+    address: {
+        city: string;
+        presentAddress: string;
+        permanentAddress?: string;
+    }
+}
+
+const user: User = {
+    name: "Parsian",
+    address:{
+        city: "Dk",
+        presentAddress: "Dk"
+    }
+}
+
+const permanentAddress = user?.address?.permanentAddress ?? "No Permanent Address";
+console.log({permanentAddress});
+
+
+
+
+
+
+
+}
