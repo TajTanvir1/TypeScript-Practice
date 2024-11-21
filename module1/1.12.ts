9d8239363f1f13e6760c102560f922474d9de52b
Tanvir Ahamed <155292317+TajTanvir1@users.noreply.github.com>
2024-11-21 17:49:14 +0600

    Unknown typeof

--- module1/1.12.ts
+++ module1/1.12.ts
@@ -1,22 +1,47 @@
 {
-// Nullable types || unknown
-
-const searchName = (value: string | null) =>{
-    if(value){
-        console.log("Searching");
+    // Nullable types || unknown
+    
+    const searchName = (value: string | null) =>{
+        if(value){
+            console.log("Searching");
+        }
+        else{
+            console.log("Nothing to search");
+        }
     }
-    else{
-        console.log("Nothing to search");
+    searchName(null);
+    
+    // unknown typeof
+    
+    const getSpeedInMeterPerSecond = (value : unknown) =>{
+        if(typeof value === "number"){
+            const convertedSpeed = (value * 1000)/3600;
+            console.log(`The speed is ${convertedSpeed} ms^-1`);
+        }
+
+        else if(typeof value === "string"){
+            const [result, unit] = value.split(' ');
+            const convertedSpeed = (parseFloat(result)*1000)/3600;
+            console.log(convertedSpeed);
+
+        } else{
+            console.log("wrong input")
+        }
     }
-}
-searchName(null);
+    
+    getSpeedInMeterPerSecond(1000);
+    getSpeedInMeterPerSecond("1000 kmh");
+    
+    
 
-// unknown typeof
+    // Never
 
+ const throwError = (msg: string): never =>{
+    throw new Error(msg);
+ }
 
-
-
-
-
-
+ throwError("Error happened");
+    
+    
+    
 }
