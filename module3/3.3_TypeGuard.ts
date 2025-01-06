153d5b50cb2230fe46e23a37a317840f56cb183a
tajtanvvir1 <tanvirah1223@gmail.com>
2025-01-06 23:24:19 +0600

    In type guard

--- module3/3.3_TypeGuard.ts
+++ module3/3.3_TypeGuard.ts
@@ -19,10 +19,36 @@ console.log(result1);
 console.log(result2);
 
 
+// in guard
 
+type NormalUser = {
+    name:string;
+}
 
+type AdminUser={
+    name: string;
+    role: "admin";
+}
 
+const getUser=(user:NormalUser| AdminUser)=>{
+    if("role" in user){
+        console.log(`May name is ${user.name} and my role is ${user.role}`);
+    }else{
+        console.log(`My name is ${user.name}`);
+    }
+};
 
+const normalUser : NormalUser ={
+    name: "Normal Vai",
+} 
+
+const adminUser : AdminUser ={
+    name: "Admin vai",
+    role: "admin",
+}
+
+getUser(adminUser);
+getUser(normalUser);
 
 
 
