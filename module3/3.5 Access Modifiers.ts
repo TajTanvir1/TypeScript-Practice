bed89f86ebc50acfc8b907ccb6c696a5aeaee1e3
tajtanvvir1 <tanvirah1223@gmail.com>
2025-01-07 23:54:31 +0600

    Access added

---
+++ module3/3.5 Access Modifiers.ts
@@ -0,0 +1,32 @@
+{
+    // Access Modifiers
+
+    class BankAccount{
+        id:number;
+        name:string;
+        balance: number;
+
+        constructor(id:number, name:string, balance:number){
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+    }
+
+    const account1 = new BankAccount(11, "account1", 5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
