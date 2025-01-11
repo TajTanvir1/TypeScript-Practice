8b66fd923ef69975c94587220f49f3c5353877b2
tajtanvvir1 <tanvirah1223@gmail.com>
2025-01-11 23:39:04 +0600

    Static update

--- module3/3.7_Static.ts
+++ module3/3.7_Static.ts
@@ -2,21 +2,24 @@
     // Static
 
     class Counter {
-        count : number = 0;
-        increment(){
-            return(this.count = this.count + 1);
+        static count : number = 0;
+        static increment(){
+            return(Counter.count = Counter.count + 1);
         }
-        decrement(){
-            return (this.count = this.count - 1);
+        static decrement(){
+            return (Counter.count = Counter.count - 1);
         }
     }
 
-    const instance1 = new Counter()
-    console.log(instance1.increment());
-    console.log(instance1.increment());
-    console.log(instance1.increment());
-
-
+    // const instance1 = new Counter()
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+    
+    // const instance2 = new Counter()
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+    
 
 
 
