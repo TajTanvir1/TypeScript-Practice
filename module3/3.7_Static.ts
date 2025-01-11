807994c62a54935694a70f42c4540ca710a54f6d
tajtanvvir1 <tanvirah1223@gmail.com>
2025-01-11 23:34:16 +0600

    Static Added

---
+++ module3/3.7_Static.ts
@@ -0,0 +1,32 @@
+{
+    // Static
+
+    class Counter {
+        count : number = 0;
+        increment(){
+            return(this.count = this.count + 1);
+        }
+        decrement(){
+            return (this.count = this.count - 1);
+        }
+    }
+
+    const instance1 = new Counter()
+    console.log(instance1.increment());
+    console.log(instance1.increment());
+    console.log(instance1.increment());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
