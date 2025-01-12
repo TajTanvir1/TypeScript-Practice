fe60b448513deb643c2ee2b8d141d23e510d0dce
tajtanvvir1 <tanvirah1223@gmail.com>
2025-01-13 00:09:21 +0600

    Polymoriphism update

--- module3/3.8_Polymorphism.ts
+++ module3/3.8_Polymorphism.ts
@@ -38,11 +38,47 @@ class Shape {
     }
 }
 
+// pi * r * r
 class Circle extends Shape{
-    getArea(radius : number): number {
-        
+radius: number;
+
+constructor(radius: number){
+    super();
+    this.radius = radius;
+}
+    getArea(): number {
+        return Math.PI * this.radius * this.radius;
     }
 }
+
+
+class Rectangle extends Shape{
+height: number;
+width: number;
+
+constructor(height: number, width: number){
+    super();
+    this.height = height;
+    this.width = width;
+}
+    getArea(): number {
+        return this.height * this.width;
+    }
+}
+
+const getShapeArea =(param : Shape)=>{
+    console.log(param.getArea());
+}
+
+const shape1 = new Shape();
+const shape2 = new Circle(10);
+const shape3 = new Shape(10, 20);
+
+getShapeArea(shape1)
+getShapeArea(shape2)
+getShapeArea(shape3)
+
+
 
 
 
