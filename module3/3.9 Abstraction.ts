f9ecf6ab566b13e18577f03f2d54d710db16f790
tajtanvvir1 <tanvirah1223@gmail.com>
2025-01-13 14:46:29 +0600

    Abstract use

--- module3/3.9 Abstraction.ts
+++ module3/3.9 Abstraction.ts
@@ -1,18 +1,18 @@
 {
     // Abstraction
-    // Abstraction give us idea
+    // Abstraction give us idea to implementation
     // Abstraction can use in 2 ways
     // 1. interface, 2. abstract
 
-// Idea (abstract) with interface
+    // Idea (abstract) with interface
     interface Vehicle1 {
-        startEngine() : void;
-        stopEngine() : void;
-        move() : void;
+        startEngine(): void;
+        stopEngine(): void;
+        move(): void;
     }
 
     // Real Implementation
-    class Car implements Vehicle1{ //implement use in class
+    class Car implements Vehicle1 { //implement use in class to get value of abstract interface
         startEngine(): void {
             console.log("Starting Engine");
         }
@@ -22,17 +22,38 @@
         move(): void {
             console.log("Moving");
         }
-        test(){
+        test() {
             console.log("Testing");
         }
     }
 
+    const toyotaCar = new Car();
+    toyotaCar.startEngine();
 
 
 
+    // 2. Abstract
 
+    abstract class Car2 {
+        abstract startEngine(): void;
+        abstract stopEngine(): void;
+        abstract move(): void;.
+        test() {
+            console.log("Testing");
+        }
+    }
 
-
+    class ToyotaCar2 extends Car2{
+        startEngine(): void {
+            console.log("Starting Engine");
+        }
+        stopEngine(): void {
+            console.log("Stop Engine");
+        }
+        move(): void {
+            console.log("Moving");
+        }
+    }
 
 
 
